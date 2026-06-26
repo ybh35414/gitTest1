fc41e86e782a96479c2c444b33c86ba0d2f3f9ca
jangcw1204@naver.com <jangcw1204@naver.com>
2026-06-26 15:51:55 +0900

    jangc4

--- gitTest1/Form1.cs
+++ gitTest1/Form1.cs
@@ -21,7 +21,7 @@ namespace gitTest1
 		{
 			//gkggkgk
 			//MessageBox.Show("유병희 수정함 V1");
-			MessageBox.Show("유병희 수정함 V2 장창욱 2");
+			MessageBox.Show("유병희 수정함 V2 장창욱 22");
 			MessageBox.Show("유병희 수정함 V3");
 		}
 	}
